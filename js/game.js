a3bf7e8b1fdbf65ba6ed3dab457764f821856ca7
Jorge Teixeira de Melo <jorge.teixeira@academico.ifrn.edu.br>
2022-10-06 11:38:53 -0300

    Parte 2 em processo

--- js/game.js
+++ js/game.js
@@ -1,8 +1,44 @@
-const createCard = () => {
-    const card = document.createElement("div");
-    const front = document.createElement("div");
-    const back = document.createElement("div");
+const grid = document.querySelector(".grid");
 
-    card.className = "card";
-    front.className = ""
+const characters = [
+    "beth",
+    "jerry",
+    "jessica",
+    "morty",
+    "pessoa-passaro",
+    "pickle-rick",
+    "rick",
+    "summer",
+    "meeseeks",
+    "scroopy",
+];
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
 };
+
+const createCard = () => {
+    const card = createElement("div", "card");
+    const front = createElement("div", "face front");
+    const back = createElement("div", "face back");
+
+    front.style.backgroundImage = `url('../images/${character}.png')`;
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    return card;
+};
+
+const loadGame = () => {
+    const duplicateCharacters = [...characters, ...characters];
+
+    duplicateCharacters.forEach((character) => {
+        const card = createCard(character);
+        grid.appendChild(card);
+    });
+};
+
+loadGame();
